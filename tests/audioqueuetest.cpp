9db7cf3b7fd29ed6018e3a7e41d072ab7981d5fa
jamieboy1337 <jamieboy1337@gmail.com>
2020-02-02 20:53:49 -0800

    wrapped queue tests

--- tests/audioqueuetest.cpp
+++ tests/audioqueuetest.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+// todo: create a concurrency demo once the workflow for that is good
+
 class AudioTest : public testing::Test {
  protected:
   AudioQueue<int16_t>* q;
@@ -24,7 +26,7 @@ TEST_F(AudioTest, CheckEmptyBuffer) {
   bool success;
   int16_t testunit = q->Pop(success);
   ASSERT_FALSE(success);
-}
+}  // CheckEmptyBuffer
 
 TEST_F(AudioTest, WriteToBuffer) {
   int16_t prepped[FILL_SIZE];
@@ -37,7 +39,7 @@ TEST_F(AudioTest, WriteToBuffer) {
   for (int16_t i = 0; i < FILL_SIZE; i++) {
     ASSERT_EQ(prepped[i], q->Pop());
   }
-}
+}  // WriteToBuffer
 
 TEST_F(AudioTest, CheckMultipleFills) {
   const int ITER_COUNT = 16;
@@ -62,4 +64,43 @@ TEST_F(AudioTest, CheckMultipleFills) {
 
     ASSERT_EQ(0, q->Size());
   }
-}
+}  // CheckMultipleFills
+
+TEST_F(AudioTest, CircularReadWrite) {
+  int16_t rand_data[8192];
+  unsigned int seed = 0;
+
+  for (int16_t i = 0; i < 8192; i++) {
+    rand_data[i] = rand_r(&seed);
+  }
+
+  int16_t read_cursor = 0;
+  int16_t write_cursor = 0;
+
+  int cur_amt;
+
+  // read a random amount, write a random amount, ensure correctness.
+  while (read_cursor < 8192 && write_cursor < 8192) {
+    cur_amt = rand_r(&seed) % (1024 - (write_cursor - read_cursor));
+    cur_amt = (cur_amt + write_cursor > 8192 ? 8192 - write_cursor : cur_amt);
+
+    q->Write(rand_data + write_cursor, cur_amt);
+    write_cursor += cur_amt;
+    
+    ASSERT_EQ(q->Size(), write_cursor - read_cursor);
+
+    cur_amt = rand_r(&seed) % q->Size();
+    for (int i = 0; i < cur_amt; i++) {
+      ASSERT_EQ(rand_data[read_cursor], q->Pop());
+      read_cursor++;
+    }
+
+    ASSERT_EQ(q->Size(), write_cursor - read_cursor);
+  }
+
+  while (read_cursor < 8192) {
+    ASSERT_EQ(q->Pop(), rand_data[read_cursor++]);
+  }
+
+  ASSERT_EQ(read_cursor, write_cursor);
+}  // CircularReadWrite
