acbbac09c36d5e7aa8525a41f19590ad37b86b7a
jamieboy1337 <jamieboy1337@gmail.com>
2020-04-08 18:56:46 -0700

    finale dingo

--- experiments/texturetest/samplefrag.frag.glsl
+++ experiments/texturetest/samplefrag.frag.glsl
@@ -6,7 +6,9 @@ in vec2 texCoord;
 uniform sampler2D tex;
 uniform sampler2D textwo;
 
+uniform float time;
+
 void main() {
-  FragColor.rgb = pow(texture(tex, texCoord).rgb * texture(textwo, texCoord).rgb, vec3(2.2 / 2.2));
+  FragColor.rgb = mix(texture(tex, texCoord).rgb, texture(textwo, texCoord).rgb, vec3((cos(time * 1.414f) + 1.0f) * 0.5f));
   FragColor.a = 1.0;
 }
