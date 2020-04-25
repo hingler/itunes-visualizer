8b6ba4607d8d6bfd419deaa0e98ba1bfad0e8ffa
jamieboy1337 <jamieboy1337@gmail.com>
2020-04-24 20:05:51 -0700

    added GL variables + created uniforms and attribs

--- include/shaders/WaveShader.hpp
+++ include/shaders/WaveShader.hpp
@@ -5,12 +5,22 @@
 #include "GLFW/glfw3.h"
 #include "shaders/AudioShader.hpp"
 
+/**
+ *  The WaveShader represents audio data via a set of vertices which extend back into the scene.
+ *  You will enjoy these colors as they perform for you.
+ */ 
 class WaveShader : public AudioShader {
  public:
   WaveShader();
   void Render(GLFWwindow* window, float* sample_data, size_t length) override;
   const std::string* GetParameterNames() override;
   void SetParameter(const std::string& param_name, std::any value) override;
+
+  const int VERTEX_DEPTH = 64;      // number of vertices along Z
+  const int VERTEX_WIDTH = 1024;    // number of vertices along X
+
+  const float SPACE_WIDTH = 8.0f;   // width occupied by vertices in +/- x directions
+  const float SPACE_DEPTH = 16.0f;  // depth occupied by vertices in -Z direction
  private:
 
   // prog
@@ -21,6 +31,9 @@ class WaveShader : public AudioShader {
 
   // vao
   GLuint vao_;
+
+  // vertex buffer
+  GLuint aPos_;
 };
 
 #endif  // MULTI_COLOR_SHADER_H_
--- src/shaders/WaveShader.cpp
+++ src/shaders/WaveShader.cpp
@@ -1,6 +1,8 @@
 #include "shaders/WaveShader.hpp"
 #include <iostream>
 
+#include <vector>
+
 WaveShader::WaveShader() {
   // create program
 
@@ -12,8 +14,27 @@ WaveShader::WaveShader() {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
+  // create tex buffer
+  glGenBuffers(1, &aPos_);
+  glBindBuffer(GL_ARRAY_BUFFER, aPos_);
+
   // define geometry
 
+  std::vector<float> vertex_cache(3 * VERTEX_DEPTH * VERTEX_WIDTH, 0.0f);
+
+  int index;
+
+  for (int i = 0; i < VERTEX_DEPTH; i++) {
+    for (int j = 0; j < VERTEX_WIDTH; j++) {
+      index = (i * VERTEX_WIDTH + j) * 3;
+      vertex_cache[index] = (SPACE_WIDTH * 2.0 / VERTEX_WIDTH) * j - SPACE_WIDTH;
+      vertex_cache[index + 1] = 0.0f;
+      vertex_cache[index + 2] = -(SPACE_DEPTH / VERTEX_DEPTH) * i;
+    }
+  }
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertex_cache.size(), vertex_cache.data(), GL_STATIC_DRAW);
+  
   // not gonna gen mipmaps because its just a buffer
 
   
