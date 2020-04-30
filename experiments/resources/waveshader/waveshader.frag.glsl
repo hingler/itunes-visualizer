588d9e01a68043b9dc4cdc34d624fd65c2e82740
jamieboy1337 <jamieboy1337@gmail.com>
2020-04-29 18:34:08 -0700

    rounded up the shaders

--- experiments/resources/waveshader/waveshader.frag.glsl
+++ experiments/resources/waveshader/waveshader.frag.glsl
@@ -1,9 +1,12 @@
 #version 410 core
 
+uniform float spaceDepth;
+
 in float intensity;
+in float z_dist;
 
 out vec4 fragColor;
 
 void main() {
-  fragColor = vec4(vec3(intensity), 1.0f);
+  fragColor = vec4(vec3(((spaceDepth - z_dist) / spaceDepth)), 1.0f);
 }
--- experiments/resources/waveshader/waveshader.vert.glsl
+++ experiments/resources/waveshader/waveshader.vert.glsl
@@ -6,19 +6,23 @@ uniform sampler2D dftHistory;
 uniform float spaceWidth;
 uniform float spaceDepth;
 
+uniform vec3 camera;
+
 uniform mat4 viewMatrix;
 
 in vec4 vectorPos;
 
 out float intensity;
+out float z_dist;
 
 const float INTENSITY_MULTIPLIER = 4.0f;
 
 void main() {
-  float xSample = (vectorPos.x / (2 * spaceWidth)) + 1;
+  float xSample = 0.125 - (abs(vectorPos.x) / spaceWidth) / 8;
   float ySample = fract(texOffsetY + (vectorPos.z / spaceDepth));
-  intensity = texture(dftHistory, vec2(xSample, ySample)); // this should be fine
+  intensity = cos(xSample * 1.5708 * 32) * pow(texture(dftHistory, vec2(xSample, ySample)).r, 0.5); // this should be fine
 
+  z_dist = length(camera - vectorPos.xyz);
   // i think this is right
   gl_Position = viewMatrix * vec4(vectorPos.x, intensity * INTENSITY_MULTIPLIER, vectorPos.zw);
 
