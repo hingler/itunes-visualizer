c880e07ebdabb47e3c71713fef738d3dee1d7095
jamieboy1337 <jamieboy1337@gmail.com>
2020-05-02 16:56:48 -0700

    intensity calculation modified to avoid "overflow"

--- experiments/resources/waveshader/waveshader.vert.glsl
+++ experiments/resources/waveshader/waveshader.vert.glsl
@@ -19,9 +19,16 @@ const float INTENSITY_MULTIPLIER = 4.0f;
 
 void main() {
   float xSample = 0.125 - (abs(vectorPos.x) / spaceWidth) / 8;
-  float ySample = fract(texOffsetY + (vectorPos.z / spaceDepth));
-  intensity = cos(xSample * 1.5708 * 32) * pow(texture(dftHistory, vec2(xSample, ySample)).r, 0.5); // this should be fine
+  float ySample = fract((-1.0 / 256) + texOffsetY + (vectorPos.z / spaceDepth));
+  intensity = (cos(xSample * 1.5708 * 32) * .5 + .6) * pow(texture(dftHistory, vec2(xSample, ySample)).r, 0.5); // this should be fine
 
+  // iirc interpolation is causing the front-most sample to combine the lastest and earliest dft samples
+  // together
+
+  // offseting by 1/256 (1 / 2 * texheight) might fix that
+  // but that would require an additional uniform to handle that for an arbitrary texture size
+
+  // update: i did it and its fixed
   z_dist = length(camera - vectorPos.xyz);
   // i think this is right
   gl_Position = viewMatrix * vec4(vectorPos.x, intensity * INTENSITY_MULTIPLIER, vectorPos.zw);
